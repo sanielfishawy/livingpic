c1649eff75cc781fc78625ed235571e754c9aa2b
Sani Elfishawy <sani@sbcglobal.net>
2012-10-25 16:01:55 -0700

    commit by rake app:package

--- www/js/application.js
+++ www/js/application.js
@@ -28489,6 +28489,23 @@ getUrlParam = function(url,name) {
   };
 
 }).call(this);
+function populateDB(tx) {
+     tx.executeSql('DROP TABLE IF EXISTS DEMO');
+     tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
+     tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
+     tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+}
+
+function errorCB(err) {
+    alert("Error processing SQL: "+err.code);
+}
+
+function successCB() {
+    alert("success!");
+}
+
+// var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+// db.transaction(populateDB, errorCB, successCB);
 (function() {
 
   $(document).ready(function() {
